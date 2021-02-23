4a33b12f895e6c3cd3689963150a2277012f55ee
1715191173 <1715191173@qq.com>
2021-02-23 22:55:47 +0800

    2.23 22:55

--- Main/D6/d6/src/App.vue
+++ Main/D6/d6/src/App.vue
@@ -1,26 +1,34 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Menus :menus="menusList" sayWhat="父组件通信给子组件" @delete="deleteOne"></Menus>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Menus from "@/components/Menus";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menus,
+  },
+  data() {
+    return {
+      menusList:[
+        {id: 1, name: '鸡'},
+        {id: 2, name: '鱼'},
+        {id: 3, name: '虾'}
+      ]
+    }
+  },
+  methods: {
+    deleteOne(i) {
+      // alert(i)
+      this.menusList.splice(i, 1);
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ Main/D6/d6/src/components/Menus.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul>
+      <h2>用于检测父子组件通信</h2>
+      <h3>{{sayWhat}}</h3>
+      <li v-for="(item, i) in menus" :key="item.id">
+        <h4>{{item.name}}</h4>
+        <button @click="deleteItem(i)">删除父组件的数据</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menus",
+  data() {
+    return {
+
+    }
+  },
+  props: ['menus', 'sayWhat'],
+  methods: {
+    deleteItem(i) {
+      this.$emit('delete', i)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
